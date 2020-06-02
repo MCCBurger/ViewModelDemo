bfd8772a2dfd0a55578f7c45698720cecbc97bf9
Brian <bburger@students.mchenry.edu>
2020-06-02 11:16:44 -0500

    DataBinding Added

--- app/src/main/java/com/ebookfrenzy/viewmodeldemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/ebookfrenzy/viewmodeldemo/ui/main/MainFragment.kt
@@ -7,7 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ebookfrenzy.viewmodeldemo.R
-import kotlinx.android.synthetic.main.main_fragment.*
+//import kotlinx.android.synthetic.main.main_fragment.*
+//import androidx.lifecycle.Observer
+import androidx.databinding.DataBindingUtil
+import com.ebookfrenzy.viewmodeldemo.databinding.MainFragmentBinding
+import com.ebookfrenzy.viewmodeldemo.BR.myViewModel
 
 class MainFragment : Fragment() {
 
@@ -16,26 +20,39 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    lateinit var binding:MainFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.main_fragment,container,false)
+        binding.setLifecycleOwner(this)
+        return binding.root
+        //return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
-        resultText.text = viewModel.getResult().toString()
-        convertButton.setOnClickListener {
+        binding.setVariable(myViewModel,viewModel)
+
+        //resultText.text = viewModel.getResult().toString()
+
+        //val resultObserver = Observer<Float>{
+        //    result -> resultText.text = result.toString()
+        //}
+
+        //viewModel.getResult().observe(this,resultObserver)
+
+        /*convertButton.setOnClickListener {
             if(dollarText.text.isNotEmpty()){
                 viewModel.setAmount(dollarText.text.toString())
-                resultText.text = viewModel.getResult().toString()
+                //resultText.text = viewModel.getResult().toString()
             }
             else{
                 resultText.text = "No Value"
             }
-        }
+        }*/
     }
 
 }
